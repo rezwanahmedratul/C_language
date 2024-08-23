8075e8244e176ba502ce818c731e7dc147a25e99
rezwanahmedratul <92155569+rezwanahmedratul@users.noreply.github.com>
2024-08-23 13:47:41 +0600

    Create AreaOfTringle.c
    The code takes base and height of a triangle from the user and outputs the area of that triangle.

---
+++ AreaOfTringle.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+    float b,h,a; // b=base, h=height, a=area
+   
+    
+    printf("Enter the Base of the Triangle\n");
+    scanf("%f",&b);
+    
+    printf("Enter the Height of the Triangle\n");
+    scanf("%f",&h);
+    
+    a=.5*b*h;
+     
+    printf("The area of the Triangle is %.2f square unit\n",a);
+ 
+    return 0;
+}
