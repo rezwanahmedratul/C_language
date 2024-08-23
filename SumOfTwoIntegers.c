81a14e8ebc8505b06b9d52b71ee4c327f8dc3746
rezwanahmedratul <92155569+rezwanahmedratul@users.noreply.github.com>
2024-08-23 14:05:38 +0600

    Create SumOfTwoIntegers.c

---
+++ SumOfTwoIntegers.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+   int a,b;
+   
+   printf("Enter a number a=");
+   scanf("%d",&a);
+  
+   printf("Enter a number b=");
+   scanf("%d",&b);
+  
+   printf("\nThe sum of a+b=%d",a+b);
+   
+   return 0;
+}
