7d5d042c93f89a49c9462d569ea0bd829e9cb42c
rezwanahmedratul <92155569+rezwanahmedratul@users.noreply.github.com>
2024-09-14 19:55:52 +0600

    Create Age Calculator.c

---
+++ Class Assignments/Age Calculator.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+
+int main()
+{
+    int d,m,y,D,M,Y,DD,MM,YY,days;
+  
+    printf("Enter current date in this format DD MM YYYY.\n");
+    scanf("%d %d %d",&d,&m,&y);
+  
+ 
+    if(m>12)
+     {
+       printf("\nOoops...Wrong Input.\nMonth cannot be greater than 12.\n");
+     }
+ 
+    else if(d>31)
+     { 
+       printf("\nOoops...Wrong Input.\nThere are only 30 days in a month.\n");
+     }
+
+    else
+    {  
+       printf("\nEnter your date of birth in this format DD MM YYYY.\n");
+       scanf("%d %d %d",&D,&M,&Y);
+    
+       if(Y>y)
+         {
+            printf("\nOoops...Wrong Input.\nBirth year cannot be greater than Present year.\n");
+         }
+       
+       else if(M>12)
+         {
+            printf("\nOoops...Wrong Input.\nMonth cannot be greater than 12.\n");
+         }
+   
+       else if(D>31)
+         { 
+            printf("\nOoops...Wrong Input.\nThere are only 30 days in a month.\n");
+         }
+                  
+       else
+         {
+            days=((y*365)+(m*30)+d)-((Y*365)+(M*30)+D);
+            YY=days/365;
+            MM=(days%365)/30;
+            DD=(days&365)%30;
+       
+            printf("\nYour age is %d Years %d Months and %d Days.\n",YY,MM,DD);
+         }
+    }
+    
+    return 0;
+}
